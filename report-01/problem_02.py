3dee49ec222b2487d4ab3f2554e32f91911ab425
Ahmudul Hossain <ahmudulhossain.n5@gmail.com>
2025-05-05 09:21:08 +0600

    practices for final

--- report-01/problem_02.py
+++
@@ -1,7 +0,0 @@
-def student(**params):
-    print("Names of all parameters with their values:")
-    for key in params.keys():
-        print(f"{key} = {params[key]}")
-
-
-student(name="Ahmud", age=23, dept="CSE")
